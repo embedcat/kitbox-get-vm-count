c6a5141128621730a00b7022d7711fdc4cec23ed
kip <kip@kit-invest.ru>
2024-02-09 19:00:37 +0300

    pass filepath through argv

--- get_active_modems.py
+++ get_active_modems.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from KVApi import KVApi, APIClient
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
@@ -11,6 +12,8 @@ def count_vms(vms: list) -> int:
 
 
 def update_html(counter: int, filepath: str) -> None:
+    if not filepath:
+        return
     with open(filepath) as file:
         html = file.read()
         soup = BeautifulSoup(html, "html.parser")
@@ -27,6 +30,9 @@ if __name__ == "__main__":
     client = APIClient(company_id=os.getenv("COMPANY_ID"), user_login=os.getenv("USER_LOGIN"), user_password=os.getenv("USER_PASSWORD"))
     api = KVApi(client=client)
     response = api.get_vm_states()
-    if response and response["ResultCode"] == 0:
-        actual_count = count_vms(vms=response["VendingMachines"])
-        update_html(counter=actual_count, filepath="index.html")
+    if response:
+        if response["ResultCode"] == 0:
+            actual_count = count_vms(vms=response["VendingMachines"])
+            update_html(counter=actual_count, filepath=sys.argv[1] if len(sys.argv) > 1 else None)
+        else:
+            print(f"Error. Result code is {response["ResultCode"]}")
