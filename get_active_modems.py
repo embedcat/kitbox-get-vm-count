0dd3942095d6c222d263e4d036ec4f69b4164b32
kip <kip@kit-invest.ru>
2024-03-25 16:53:29 +0300

    сохраниение запроса во временный файл

--- get_active_modems.py
+++ get_active_modems.py
@@ -7,6 +7,9 @@ from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 
 
+TEMP_FILENAME = "vend_machines.txt"
+
+
 def count_vms(vms: list) -> int:
     now = datetime.now()
     return len([elem for elem in vms if now - datetime.strptime(elem["DateTime"], "%d.%m.%Y %H:%M:%S") <= timedelta(weeks=2)])
@@ -25,7 +28,7 @@ def update_html(counter: int, device_count: list[str], filepath: str) -> None:
     tag_devices = soup.find(id="devices")
     tag_devices.clear()
     for item in device_count:
-        li = soup.new_tag("li", attrs={"class": "device-item"}) 
+        li = soup.new_tag("li", attrs={"class": "device-item"})
         li.string = item
         tag_devices.append(li)
     with open(filepath, "w") as file:
@@ -36,9 +39,12 @@ if __name__ == "__main__":
     load_dotenv(override=True)
     client = APIClient(company_id=os.getenv("COMPANY_ID"), user_login=os.getenv("USER_LOGIN"), user_password=os.getenv("USER_PASSWORD"))
     api = KVApi(client=client)
-    
+
     script_path = os.path.dirname(os.path.abspath(__file__))
-    file = f"{script_path}/company{client.company_id}_{datetime.strftime(datetime.now(), '%d_%m_%Y_%H_%M_%S')}_vm_states.txt"
+    # file = f"{script_path}/company{client.company_id}_{datetime.strftime(datetime.now(), '%d_%m_%Y_%H_%M_%S')}_vm_states.txt"
+    file = f"{script_path}/{TEMP_FILENAME}"
+    if os.path.exists(file):
+        os.remove(file)
 
     response = api.get_vm_states(file_path_to_dump=file)
     if response:
